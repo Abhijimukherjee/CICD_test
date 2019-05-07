68e3c09346d7aacd37c92a75f3e184fc430ed515
ashishsingh8 <ashishsingh8@deloitte.com>
2019-05-07 15:08:08 +0530

    Testing code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { testcomponent } from '../components/testcomponent/testcomponent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'test',
+    component: testcomponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
